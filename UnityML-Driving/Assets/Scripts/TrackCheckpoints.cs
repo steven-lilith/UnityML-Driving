acf91195c1f0065235762808199c679906e88a32
william chen <53885588+YourFather69420@users.noreply.github.com>
2022-02-23 17:35:22 -0700

    added some comments

--- UnityML-Driving/Assets/Scripts/TrackCheckpoints.cs
+++ UnityML-Driving/Assets/Scripts/TrackCheckpoints.cs
@@ -11,6 +11,7 @@ public class TrackCheckpoints : MonoBehaviour {
     {
         Transform checkpointTransform = transform.Find("CheckPoints");
         checkpointSinglesList = new List<CheckpointSingle>();
+        //check the check points within the list in correct order
         foreach (Transform checkpointSingleTransform in checkpointTransform)
         {
             CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
@@ -20,11 +21,13 @@ public class TrackCheckpoints : MonoBehaviour {
     }
     public void GoThroughCheckPoint(CheckpointSingle checkpointSingle)
     {
+        //check if the player has skipped any check points 
         if(checkpointSinglesList.IndexOf(checkpointSingle)==nextIndex)
         {
             Debug.Log("correct");
-            nextIndex = (nextIndex + 1) % checkpointSinglesList.Count;
+            nextIndex = (nextIndex + 1) % checkpointSinglesList.Count; //make sure all the check points are available for the second lap
         }
+        //if yes, then addreward(-1f)
         else
         {
             Debug.Log("wrong");
